f2b9637f22ea9fbe073296a309215d1be7b1cb6b
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-06-12 21:38:02 +0600

    Update port configuration to use environment variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = process.env.MONGODB_CONNECT_URI;
